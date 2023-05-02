c2e2c990727efaa0afb5d393fb78c3d18b6ced81
samimhossain <samimhossainsanto@gmail.com>
2023-05-03 03:51:42 +0600

    chefData done

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const port = 5000;
 
+const Chef = require("./Data/ChefData.json");
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(Chef);
+});
+
+app.get("/ChefCartDetails/:id", (req, res) => {
+  const id = req.params.id;
+  const SelectedChef = Chef.find((chef) => chef.id === id);
+  res.send(SelectedChef);
 });
 
 app.listen(port, () => {
