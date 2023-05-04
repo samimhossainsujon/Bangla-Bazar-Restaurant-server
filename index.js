241a1a7a44058dd85fa13c5a11cf906a5e7bf2d8
samimhossain <samimhossainsanto@gmail.com>
2023-05-04 16:10:37 +0600

    section 2  data done

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ app.use(cors());
 const port = 5000;
 
 const Chef = require("./Data/ChefData.json");
+const SectionData1 = require("./Data/SectionData1.json");
+const SectionData2 = require("./Data/SectionData2.json");
+
+
+
 app.get("/", (req, res) => {
   res.send(Chef);
 });
+
+
+app.get("/section1", (req, res) => {
+  res.send(SectionData1);
+});
+
+
+app.get("/section2", (req, res) => {
+  res.send(SectionData2);
+});
+
+
+
 
 app.get("/ChefCartDetails/:id", (req, res) => {
   const id = req.params.id;
